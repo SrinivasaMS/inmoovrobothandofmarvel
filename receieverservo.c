9c9d8e1ed963dc4d01884cc8cf7dd08d70179285
Srinivasa M S <78272767+SrinivasaMS@users.noreply.github.com>
2023-09-12 23:19:54 +0530

    Update receieverservo.c
    Updated code which is compiled properly

--- receieverservo.c
+++ receieverservo.c
@@ -1,73 +1,92 @@
+#include <ESP32_Servo.h>
+
+//Receiver Code
 #include <esp_now.h>
 #include <WiFi.h>
-#include <Servo.h>
 
-#define THUMB_PIN_SERVO    12     
-#define INDEX_PIN_SERVO    13    
-#define MIDDLE_PIN_SERVO   14      
-#define RING_PIN_SERVO     15   
-#define PINKY_PIN_SERVO    04 
+//Servo objects
+Servo Servomotor1;
+Servo Servomotor3;
+Servo Servomotor4;
+Servo Servomotor2;
+Servo Servomotor5;
 
-Servo myServo1;
-Servo myServo2;
-Servo myServo3;
-Servo myServo4;
-Servo myServo5;
+// servo pin declaration
+int thumbservopin = 35; 
+int indexservopin = 34; 
+int middleservopin = 33; 
+int ringservopin = 32; 
+int pinkyservopin = 25;
 
-int angles[5];
+// Structure example to receive data
+// Must match the sender structure
+typedef struct struct_message {
+int a;
+int b;
+int c;
+int d;
+int e;
+} struct_message;
 
-uint8_t broadcastAddress[] = {0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF};
-
+// Create a struct_message called myData
+struct_message myData;
 
 // callback function that will be executed when data is received
-void OnDataRecv(const uint8_t * mac, const uint8_t *data, int data_len) {
-
-    Serial.print("Recieved data: ");
-    for (int i = 0; i < data_len; i++ ){
-      angle[i] = data[i];
-    }
-
+void OnDataRecv(const uint8_t * mac, const uint8_t *incomingData, int len) {
+memcpy(&myData, incomingData, sizeof(myData));
+Serial.print("Bytes received: ");
+Serial.println(len);
+Serial.print("int: ");
+Serial.println(myData.a);
+Serial.print("int: ");
+Serial.println(myData.b);
+Serial.print("int: ");
+Serial.println(myData.c);
+Serial.print("int: ");
+Serial.println(myData.d);
+Serial.print("int: ");
+Serial.println(myData.e);
+Serial.println();
 }
- 
+
+
+
+int analogthumb = myData.a;
+int analogindex = myData.b;
+int analogmiddle = myData.c;
+int analogring = myData.d;
+int analogpinky = myData.e;
+
 void setup() {
-  // Initialize Serial Monitor
-  Serial.begin(115200);
-  
-  // Set device as a Wi-Fi Station
-  WiFi.mode(WIFI_STA);
 
-  // Init ESP-NOW
-  if (esp_now_init() != ESP_OK) {
-    Serial.println("Error initializing ESP-NOW");
-    return;
-  }
-  
-  // Once ESPNow is successfully Init, we will register for recv CB to
-  // get recv packer info
-  esp_now_register_recv_cb(OnDataRecv); 
-  myServo1.attach(THUMB_PIN_SERVO);
-  myServo2.attach(INDEX_PIN_SERVO);
-  myServo3.attach(MIDDLE_PIN_SERVO);
-  myServo4.attach(RING_PIN_SERVO);
-  myServo5.attach(PINKY_PIN_SERVO);  // attaches ESP32 pin to the servo object
+//Naampies servo motor input pins INPUT PINS AANPASSEN VANWEGE VERANDERINGEN
+Servomotor1.attach(18); //A1 D18
+Servomotor2.attach(4); //A2 D4
+Servomotor3.attach(5); //A3 D5
+Servomotor4.attach(21); //A4 D21
+Servomotor5.attach(19); //A5 D19
 
+// Initialize Serial Monitor
+Serial.begin(115200);
 
+// Set device as a Wi-Fi Station
+WiFi.mode(WIFI_STA);
+
+// Init ESP-NOW
+if (esp_now_init() != ESP_OK) {
+Serial.println("Error initializing ESP-NOW");
+return;
 }
- 
-void loop() {
-  // sets the servo position according to the scaled value
-  myServo1.write(angle[1]);
-  myServo2.write(angle[2]);
-  myServo3.write(angle[3]);
-  myServo4.write(angle[4]);
-  myServo5.write(angle[5]);
 
-  
-  Serial.println(angle[1]);
-  Serial.println(angle[2]);
-  Serial.println(angle[3]);
-  Serial.println(angle[4]);
-  Serial.println(angle[5]);
-  delay(100);
+// Once ESPNow is successfully Init, we will register for recv CB to
+// get recv packer info
+esp_now_register_recv_cb(OnDataRecv);
+}
+void loop(){
 
+Servomotor1.write(analogthumb); 
+Servomotor2.write(analogindex)); 
+Servomotor3.write(analogmiddle));
+Servomotor4.write(analogring));
+Servomotor5.write(analogpinky)); 
 }
