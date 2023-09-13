02eaecb8af4084008ef346065ca9dbd50009c9a3
Srinivasa M S <78272767+SrinivasaMS@users.noreply.github.com>
2023-09-13 06:43:14 +0530

    Update reciever_connected_to_servoMAC_ADDRESS.c
    Code has description on which esp32 address we need.

--- reciever_connected_to_servoMAC_ADDRESS.c
+++ reciever_connected_to_servoMAC_ADDRESS.c
@@ -1,10 +1,15 @@
-// Complete Instructions to Get and Change ESP #include "WiFi.h"
- void setup(){ 
- Serial.begin(115200);
-  WiFi.mode(WIFI_MODE_STA);     Serial.println(WiFi.macAddress());
-   }
-
-
- void loop(){
+// the address of the esp32 connected to servo
+#include "WiFi.h"
+void setup() { 
  
-  }
+ Serial.begin(115200);
+ WiFi.mode(WIFI_MODE_STA);     Serial.println(WiFi.macAddress());
+ 
+}
+
+void loop()
+{
+ 
+}
+
+// use this address for the address which is asked in other programs
